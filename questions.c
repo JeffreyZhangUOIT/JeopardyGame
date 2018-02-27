e0e37c08032deb423fb28c426b6c65f3a0ac8f50
Jehro Lovil Celemin <Jr4dz@users.noreply.github.com>
2018-02-26 22:04:39 -0500

    Update questions.c

--- questions.c
+++ questions.c
@@ -3,7 +3,7 @@
 char* question, answer;
 
 // Grabs question. Returns error or the question itself as a char*.
-char* getQuestion(char* player, char* category, char* value) {
+char* getQuestion(char* category, char* value) {
 	FILE* fp;
 	char* line;
 	line = malloc(100 * sizeof(char));
@@ -57,4 +57,111 @@ int checkAnswer(char* playerAnswer) {
 // Ditto.
 char* getAnswer() {
 	return answer;
+}
+
+void initialize_game(void)
+{
+	strcpy(questions[0].question, getQuestion(programming, 100);
+	strcpy(questions[0].answer, getAnswer());
+	strcpy(questions[1].question, getQuestion(programming, 200);
+	strcpy(questions[1].answer, getAnswer());
+	strcpy(questions[2].question, getQuestion(programming, 300);
+	strcpy(questions[2].answer, getAnswer());
+	strcpy(questions[3].question, getQuestion(programming, 400);
+	strcpy(questions[3].answer, getAnswer());
+	strcpy(questions[4].question, getQuestion(programming, 100);
+	strcpy(questions[4].answer, getAnswer());
+	strcpy(questions[5].question, getQuestion(programming, 200);
+	strcpy(questions[5].answer, getAnswer());
+	strcpy(questions[6].question, getQuestion(programming, 300);
+	strcpy(questions[6].answer, getAnswer());
+	strcpy(questions[7].question, getQuestion(programming, 400);
+	strcpy(questions[7].answer, getAnswer());
+	strcpy(questions[8].question, getQuestion(programming, 100);
+	strcpy(questions[8].answer, getAnswer());
+	strcpy(questions[9].question, getQuestion(programming, 200);
+	strcpy(questions[9].answer, getAnswer());
+	strcpy(questions[10].question, getQuestion(programming, 300);
+	strcpy(questions[10].answer, getAnswer());
+	strcpy(questions[11].question, getQuestion(programming, 400);
+	strcpy(questions[11].answer, getAnswer());
+}
+	       
+// display remaining questions and corresponding values
+void display_categories(void)
+{
+	char val[12][20];
+
+	for (int i = 0; i < 12; i++) {
+		if (questions[i].answered == false) {
+			snprintf(val[i], 20, "%d", questions[i].value);
+		} 
+		else {
+			strcpy(val[i], "   ");
+		}
+	}
+	
+	// formatting to make it look nice
+	printf("   %s   |   %s   |   %s   \n", categories[0], categories[1], categories[2]);
+	printf("  -------------------------------------------------\n");
+	printf("      %s      |     %s     |         %s   \n", val[0], val[1],val[2]);
+	printf("  -------------------------------------------------\n");
+	printf("      %s      |     %s     |         %s   \n", val[3], val[4], val[5]);
+	printf("  -------------------------------------------------\n");
+	printf("      %s      |     %s     |         %s   \n", val[6], val[7], val[8]);
+	printf("  -------------------------------------------------\n");
+	printf("      %s      |     %s     |         %s   \n", val[9], val[10], val[11]);
+	printf("  -------------------------------------------------\n");
+}
+	      
+// Displays question and its corresponding point value
+void display_question(char *category, int value)
+{
+	for (int i = 0; i < 12; i++) {
+		if ((questions[i].category == category) && (questions[i].value == value)) {
+			printf("Question: %s (%d)\n", questions[i].question, questions[i].value);
+		}
+	}
+}
+	       
+// Returns true if reply matches answer for corresponding category and value
+bool valid_answer(char *category, int value, char *answer)
+{
+	for (int i = 0; i < 12; i++) {
+		if ((questions[i].category == category) && (questions[i].value == value)) {
+			if (strcmp(questions[i].answer, answer) == 0) {
+				return true;
+				break;
+			} 
+			else {
+				return false;
+			}
+		}
+	}
+}
+	       
+// Returns true if the question has already been answered
+bool already_answered(char *category, int value)
+{
+	for (int i = 0; i < 12; i++) {
+		if ((questions[i].category == category) && (questions[i].value == value)) {
+			if (questions[i].answered == true) {
+				rturn true;
+				break;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+}
+
+// Updates boolean value in questions struct
+void question_answered(char *category, int value)
+{
+	for (int i = 0; i < 12; i++) {
+		if ((questions[i].category == category) && (questions[i].value == value)) {
+			questions[i].answered = true;
+		}
+	}
 }
